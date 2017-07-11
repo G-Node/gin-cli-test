7955c24ae28fb98b09d4bca963f859ec56fc10de
Achilleas Koutsou <achilleas.k@gmail.com>
2017-07-11 17:10:26 +0200

    Cleanup script: Delete all test repos from the server

---
+++ delete-all-test-repos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+source ./setenv.sh
+
+gin login $username <<< $password
+
+# collect all test repo names
+testrepos=$(gin repos | grep -oP "\w+/gin-test-\d+")
+echo "The following repositories will be deleted"
+for reponame in $testrepos
+do
+    echo -e "\t $reponame"
+    done
+echo "Ctrl+C cancels"
+read
+
+# deleting
+for reponame in $testrepos
+do
+    gin delete $reponame <<< $reponame
+done
+
