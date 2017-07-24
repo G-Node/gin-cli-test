60a9e9cbf555d35289251b3c63bf719cc5d040c1
Achilleas Koutsou <achilleas.k@gmail.com>
2017-07-24 17:23:07 +0200

    Updating that delete regex again

--- delete-all-test-repos.sh
+++ delete-all-test-repos.sh
@@ -6,7 +6,7 @@ source ./setenv.sh
 gin login $username <<< $password
 
 # collect all test repo names
-testrepos=$(gin repos | grep -o "\w\+/gin-test-\(win-\)\?\d\+")
+testrepos=$(gin repos | grep -o "\w\+/gin-test-\(win-\)\?[0-9]\+")
 echo "The following repositories will be deleted"
 for reponame in $testrepos
 do
