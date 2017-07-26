3ac082b51de74ff3e1703c7f2f1bd017b2fdb274
Achilleas Koutsou <achilleas.k@gmail.com>
2017-07-26 19:49:39 +0200

    Copy of all-states test for direct mode

---
+++ all-states-dirtree-direct.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+source ./setenv.sh
+
+testroot="/tmp/gintest"
+mkdir -p "$testroot"
+cd "$testroot"
+
+gin login $username <<< $password
+
+# create repo (remote and local) and cd into directory
+reponame=gin-test-${RANDOM}
+repopath=${username}/${reponame}
+gin create $reponame "Test repository --- Created with test scripts"
+pushd $reponame
+
+echo "************ SWITCHING TO DIRECT MODE ************"
+git annex direct
+
+# create files in root
+for idx in {070..090}
+do
+    fname=root-$idx.annex
+    echo "I am root file $idx, added to annex" > $fname
+    git annex add $fname
+done
+
+[ $(gin ls --short | grep -F "LC" | wc -l) -eq 21 ]
+
+git annex proxy -- git commit -m "adding stuff"
+[ $(gin ls --short | grep -F "LC" | wc -l) -eq 21 ]
+
+gin upload
+[ $(gin ls --short | grep -F "OK" | wc -l) -eq 21 ]
+
+# gin upload command should not have created an extra commit
+[ $(git --no-pager log | grep "^commit" | wc -l) -eq 2 ]
+
+# Create more root files that will remain UNTRACKED
+for idx in {a..f}
+do
+    fname=root-file-$idx.untracked
+    echo "I am a root file. I will not be added to git or annex" > $fname
+done
+
+# Create some subdirectories with files
+for idx in {a..f}
+do
+    dirname=subdir-$idx
+    mkdir -v $dirname
+    pushd $dirname
+    for jdx in {01..10}
+    do
+        fname=subfile-$jdx.annex
+        echo "I am file $jdx in directory $dirname" > $fname
+    done
+    popd
+done
+
+# Upload the files of the first subdirectory only and a couple from the second
+gin upload subdir-a subdir-b/subfile-05.annex subdir-b/subfile-10.annex
+
+# should only have 12 new synced files
+[ $(gin ls --short | grep -F "OK" | wc -l) -eq 33 ]
+# there should be 54 untracked files total
+[ $(gin ls --short | grep -F "??" | wc -l) -eq 54 ]
+# can also check each directory individually
+[ $(gin ls --short subdir-b | grep -F "??" | wc -l) -eq 8 ]
+for idx in {c..f}
+do
+    dirname=subdir-$idx
+    [ $(gin ls --short $dirname | grep -F "??" | wc -l) -eq 10 ]
+done
+
+# There should be no NC files so far
+[ $(gin ls --short subdir-b | grep -F "NC" | wc -l) -eq 0 ]
+
+# drop some files and check the counts
+gin rmc subdir-b/subfile-05.annex
+[ $(gin ls --short subdir-b | grep -F "NC" | wc -l) -eq 1 ]
+
+gin rmc subdir-b
+[ $(gin ls --short subdir-b | grep -F "NC" | wc -l) -eq 2 ]
+
+gin remove-content subdir-a
+[ $(gin ls --short subdir-b | grep -F "NC" | wc -l) -eq 2 ]
+[ $(gin ls --short subdir-a | grep -F "NC" | wc -l) -eq 10 ]
+[ $(gin ls -s | grep -F "NC" | wc -l) -eq 12 ]
+
+# NC files are broken symlinks
+[ $(find . -xtype l | wc -l) -eq 12 ]
+
+# cleanup
+git annex uninit || true
+popd
+rm -rf $repopath
+gin delete $repopath <<< $repopath
+
+
+echo "DONE!"
