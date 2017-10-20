371536b9db98f6082cd2c5943e00eb98f9ca5759
Achilleas Koutsou <achilleas.k@gmail.com>
2017-10-20 13:00:32 +0200

    Attach test directory to container

--- start-server.sh
+++ start-server.sh
@@ -1,2 +1,2 @@
 loc=$(cd $(dirname $0) && pwd)
-docker run -v "${loc}/gin-data/":/data -p 3000:3000 -p 2222:22 --name gintest -d gnode/ginhome
+docker run -v "${loc}/gin-data/":/data -v "${loc}":/root/tests -p 3000:3000 -p 2222:22 --name gintest -d gnode/ginhome
