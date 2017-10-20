72883a7525265f2d01753ca1789217ed4c43f23a
Achilleas Koutsou <achilleas.k@gmail.com>
2017-10-20 13:29:03 +0200

    Caller script: Run each test in container and check for errors

---
+++ run-tests-in-container.sh
@@ -0,0 +1,37 @@
+loc=$(cd $(dirname $0) && pwd)
+cd $loc
+
+errorcount=0
+errored=()
+
+runscript() {
+    docker exec -i -t gintest bash -c "cd /root/tests/; PATH=\$PATH:/root/tests/bin ./$1"
+}
+
+for testscript in test-*.sh
+do
+    runscript $testscript
+    teststatus=$?
+    if [ $teststatus -ne 0 ]
+    then
+        echo "Test $testscript failed"
+        errorcount=$((errorcount+1))
+        errored+=("$testscript")
+    fi
+done
+
+runscript ./logout.sh
+
+if [ $errorcount -gt 0 ]
+then
+    echo "------------"
+    echo "Tests failed"
+    echo "------------"
+    for testscript in "${errored[@]}"
+    do
+        echo $testscript
+    done
+    exit 1
+fi
+
+echo "All tests succeeded"
