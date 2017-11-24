63bfbab72af379c4dffac520950e84206426217b
Test user <test@example.com>
2017-11-24 18:55:09 +0100

    Delete leftover files in local store
    Permission issues might keep large files around after performing a
    gin delete.

--- delete-all-test-repos.sh
+++ delete-all-test-repos.sh
@@ -6,19 +6,42 @@ source ./setenv.sh
 gin login $username <<< $password
 
 # collect all test repo names
-testrepos=$(gin repos | grep -o "\w\+/gin-test-\(win-\)\?[0-9]\+")
-echo "The following repositories will be deleted"
-for reponame in $testrepos
-do
-    echo -e "\t $reponame"
+if testrepos=$(gin repos | grep -o "\w\+/gin-test-\(win-\)\?[0-9]\+")
+then
+    echo "The following repositories will be deleted"
+    for reponame in $testrepos
+    do
+        echo -e "\t $reponame"
     done
-echo "Ctrl+C cancels"
-read
+    echo "Ctrl+C cancels"
+    read
+fi
 
 # deleting
 for reponame in $testrepos
 do
     gin delete $reponame <<< $reponame
 done
+
+repostore="./gin-data/gogs-repositories/$username/"
+if compgen -G "$repostore/gin-test-*" > /dev/null
+then
+    testrepos="$repostore/gin-test-*"
+    echo "Deleting leftover files in the following directories"
+    for dirname in $testrepos
+    do
+        echo -e "\t $dirname"
+    done
+    echo "Ctrl+C cancels"
+    read
+
+    # chmod and delete
+    for reponame in $testrepos
+    do
+        chmod 777 -R $reponame
+        rm -r $reponame
+        echo "Removed $reponame"
+    done
+fi
 
 echo "DONE!"
