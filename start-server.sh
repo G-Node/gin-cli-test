014595725b0556e7fa4209cb7d75fca423a15351
Achilleas Koutsou <achilleas.k@gmail.com>
2017-12-15 18:40:51 +0100

    Copy gin-data.init to gin-data when running server

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,6 @@
 loc=$(cd $(dirname $0) && pwd)
 set -x
+
+cp -a "${loc}/gin-data.init/." "${loc}/gin-data"
+
 docker run --rm -v "${loc}/gin-data/":/data -v "${loc}":/root/tests -p 3000:3000 -p 2222:22 --name gintest -d gnode/ginhome
--- stop-server.sh
+++ stop-server.sh
@@ -2,3 +2,5 @@ loc=$(cd $(dirname $0) && pwd)
 set -x
 docker kill gintest
 docker container rm gintest
+
+rm -r "${loc}/gin-data"
