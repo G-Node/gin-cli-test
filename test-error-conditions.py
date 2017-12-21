30a13c535249aa622b49b53454da99e9f76e9d68
Achilleas Koutsou <achilleas.k@gmail.com>
2017-12-21 14:46:59 +0100

    Test for most error conditions

--- test-error-conditions.py
+++ test-error-conditions.py
@@ -1,44 +1,131 @@
 #!/usr/bin/env python
+import os
 from random import randint
 from runner import Runner
+import util
 
 
-norepoerr = "ERROR This command must be run from inside a gin repository."
+def main():
+    norepoerr = "ERROR This command must be run from inside a gin repository."
 
-r = Runner()
+    r = Runner()
 
-r.login()
-username = r.username
+    r.login()
+    username = r.username
 
-commands = [
-    "upload",
-    "download",
-    "lock",
-    "unlock",
-    "get-content",
-    "remove-content",
-]
+    commands = [
+        "upload",
+        "download",
+        "lock",
+        "unlock",
+        "get-content",
+        "remove-content",
+    ]
 
-for cmd in commands:
-    out, err = r.runcommand("gin", cmd, ".", exit=False)
-    assert err == norepoerr, f"Unexpected error output {err}"
+    # Unable to run any git commands outside a git repository
+    for cmd in commands:
+        # On directory
+        out, err = r.runcommand("gin", cmd, ".", exit=False)
+        assert err == norepoerr, f"Unexpected error output {err}"
+
+        # On specific file
+        out, err = r.runcommand("gin", cmd, "foobar", exit=False)
+        assert err == norepoerr, f"Unexpected error output {err}"
+
+    # create repo (remote and local) and cd into directory
+    reponame = f"gin-test-{randint(0, 9999):04}"
+    repopath = f"{username}/{reponame}"
+    r.runcommand("gin", "create", reponame,
+                 "Test repository for error output. Created with test scripts")
+    r.cdrel(reponame)
+
+    # Unable to run any command on file that does not exist (download ignored)
+    out, err = r.runcommand("gin", "upload", "foobar", exit=False)
+    assert out == "Error: No files matched foobar"
+    assert err == "ERROR 1 operation failed", f"Unexpected error output {err}"
+    for cmd in commands[2:]:
+        out, err = r.runcommand("gin", cmd, "foobar", exit=False)
+        assert out == "Error: No files matched foobar"
+        assert err == "ERROR 1 operation failed", (f"Unexpected error output"
+                                                   "{err}")
+
+    # make a few annex and git files
+    os.mkdir("smallfiles")
+    r.cdrel("smallfiles")
+    for idx in range(20):
+        util.mkrandfile(f"smallfile-{idx:3}", 20)
+    r.cdrel("..")
+    os.mkdir("datafiles")
+    r.cdrel("datafiles")
+    for idx in range(5):
+        util.mkrandfile(f"datafile-{idx:3}", 2000)
+    r.cdrel("..")
+
+    # Unable to lock, unlock, getc, rmc on untracked file(s)
+    for cmd in commands[2:]:
+        out, err = r.runcommand("gin", cmd, "smallfiles", exit=False)
+        assert err, "Expected error, got nothing"
+        for line in err.splitlines():
+            assert line.strip().endswith("is not under gin control")
+
+        out, err = r.runcommand("gin", cmd, "datafiles", exit=False)
+        assert err, "Expected error, got nothing"
+        for line in err.splitlines():
+            assert line.strip().endswith("is not under gin control")
+
+        out, err = r.runcommand("gin", cmd, "datafiles/*", exit=False)
+        assert err, "Expected error, got nothing"
+        for line in err.splitlines():
+            assert line.strip().endswith("is not under gin control")
+
+    out, err = r.runcommand("gin", "upload", "smallfiles")
+    assert not err, f"Unexpected error output {err}"
+
+    # Unable to lock, unlock, getc, rmc on files in git (not annex)
+    for cmd in commands[2:]:
+        out, err = r.runcommand("gin", cmd, "smallfiles", exit=False)
+        assert err, "Expected error, got nothing"
+        assert err.contains("cannot remove content")
+
+    out, err = r.runcommand("gin", "upload", "datafiles")
+    out, err = r.runcommand("gin", "rmc", "datafiles")
+
+    # Unable to unlock without content
+    out, err = r.runcommand("gin", "unlock", "datafiles", exit=False)
+    for line in err.splitlines():
+        assert line.strip().endswith("Content not available locally")
+
+    # TODO: Change server address and try go get-content
+
+    # TODO: Figure out how to simulate not-enough-free-space
+
+    # Creating repository that already exists
+    r.cdrel("..")
+
+    out, err = r.runcommand("gin", "create", reponame, exit=False)
+    assert err == f"Repository {repopath} already exists"
+
+    anotherrepo = f"gin-test-{randint(0, 9999):04}"
+    os.mkdir(anotherrepo)
+
+    # Creating repository but local directory already exists
+    out, err = r.runcommand("gin", "create", anotherrepo, exit=False)
+    assert (out.contains("Creating repository") and
+            out.contains("OK")), f"Failed to create {anotherrepo} on server"
+    assert err == (
+        f"Failed to download. Directory '{anotherrepo}' "
+        "already exists and is not empty."
+    )
+
+    out, err = r.runcommand("gin", "repos")
+    assert out.contains(anotherrepo), (f"{anotherrepo} was not "
+                                       "created on the server")
+
+    r.cleanup(reponame)
+    r.logout()
+
+    print("DONE!")
 
 
-out, err = r.runcommand("gin", "lock", "foobar", exit=False)
-assert err == norepoerr, f"Unexpected error output {err}"
-
-# create repo (remote and local) and cd into directory
-reponame = f"gin-test-{randint(0, 9999):04}"
-repopath = f"{username}/{reponame}"
-r.runcommand("gin", "create", reponame,
-             "Test repository for error output -- Created with test scripts")
-r.cdrel(reponame)
-
-out, err = r.runcommand("gin", "lock", "foobar", exit=False)
-assert out == "Error: No files matched foobar"
-assert err == "ERROR 1 operation failed", f"Unexpected error output {err}"
-
-r.cleanup(reponame)
-r.logout()
-
-print("DONE!")
+if __name__ == "__main__":
+    main()
