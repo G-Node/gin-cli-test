81ade45da9b212001ae6dcc020f938fdc4e428b4
Achilleas Koutsou <achilleas.k@gmail.com>
2018-02-15 16:40:33 +0100

    Attempt to match the calling user's UID inside test container

--- run-cont-tests.sh
+++ run-cont-tests.sh
@@ -5,5 +5,5 @@ set -xeu
 loc=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 pushd $loc
 
-docker build -t ginclitests ginclitests
+docker build --build-arg UID=${UID} -t ginclitests ginclitests
 docker run --rm --network=ginbridge -v "${loc}/scripts/":/home/ginuser/scripts -v "${loc}/bin/":/ginbin --name gintestclient ginclitests
