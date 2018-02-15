3b330c53e978eb275d99b8ad3eba0955025bb98e
Achilleas Koutsou <achilleas.k@gmail.com>
2018-02-15 14:00:40 +0100

    Mount scripts into user home directory

--- run-cont-tests.sh
+++ run-cont-tests.sh
@@ -6,4 +6,4 @@ loc=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 pushd $loc
 
 docker build -t ginclitests ginclitests
-docker run --rm --network=ginbridge -v "${loc}/scripts/":/scripts -v "${loc}/bin/":/ginbin --name gintestclient ginclitests
+docker run --rm --network=ginbridge -v "${loc}/scripts/":/home/ginuser/scripts -v "${loc}/bin/":/ginbin --name gintestclient ginclitests
