ca734660ff4085da174823966f79060a58fae5f6
Achilleas Koutsou <achilleas.k@gmail.com>
2018-02-16 13:17:00 +0100

    Save the test output to a log

--- run-cont-tests.sh
+++ run-cont-tests.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-set -xeu
+set -eu
 
 loc=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 pushd $loc
 
 docker build --build-arg UID=${UID} -t ginclitests ginclitests
-docker run --rm --network=ginbridge -v "${loc}/scripts/":/home/ginuser/scripts -v "${loc}/bin/":/ginbin --name gintestclient ginclitests
+
+source ./scripts/setenv.sh
+testlog=${GIN_LOG_DIR}/tests.log
+echo "Running tests and logging to ${testlog}"
+echo $(pwd)
+docker run --rm --network=ginbridge -v "${loc}/scripts/":/home/ginuser/scripts -v "${loc}/bin/":/ginbin --name gintestclient ginclitests &> ${testlog}
