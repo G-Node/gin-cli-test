0e581e21b1c191a90b513f286c42ff5d86c7d983
Achilleas Koutsou <achilleas.k@gmail.com>
2018-02-23 17:45:40 +0100

    [run-cont.sh] Set gin custom env variables in cont

--- run-cont.sh
+++ run-cont.sh
@@ -3,6 +3,7 @@
 # Use this script to start the test runner container in the background and execute any command that is supplied as an argument.
 # The container is killed at the end of the run.
 # The script blindly passes whatever arguments are supplied to the container as a command, so proper command syntax and paths are required.
+# Env variables are set to use the tester config and log directories for gin-cli.
 
 cmd=$*
 
@@ -15,7 +16,7 @@ docker build --build-arg UID=${UID} -t ginclitests dockerfiles/tester
 docker run --rm --network=ginbridge -v "${loc}/testuserhome":/home/ginuser -v "${loc}/scripts/":/home/ginuser/scripts -v "${loc}/bin/":/ginbin -i --name gintestclient --entrypoint="/usr/bin/bash" -d ginclitests
 
 set +e
-docker exec gintestclient $cmd
+docker exec --env="GIN_CONFIG_DIR=/home/ginuser/scripts/conf" --env="GIN_LOG_DIR=/home/ginuser/scripts/log" gintestclient $cmd
 cmdstat=$?
 
 docker kill gintestclient
