17a2096e889df1854d3b5d53749ad377bfdb91d0
Achilleas Koutsou <achilleas.k@gmail.com>
2018-04-16 11:15:01 +0200

    Use /tmp for "live" server data

--- start-server.sh
+++ start-server.sh
@@ -5,10 +5,14 @@ pushd $loc
 
 set -xeu
 
+# clean up old stuff from potentially improper server shutdown
+docker network rm ginbridge || true
+
 docker pull gnode/ginhome
 
 # copy init server data to live location
-cp -a "./gin-data.init/." "./gin-data"
+srvdata="/tmp/gin-data-${RANDOM}"
+cp -a "./gin-data.init/." "${srvdata}"
 
 docker network create -d bridge ginbridge
-docker run --rm --network=ginbridge -v "${loc}/gin-data/":/data -p 3000:3000 -p 2222:22 --name gintestserver -d gnode/ginhome
+docker run --rm --network=ginbridge -v "${srvdata}":/data -p 3000:3000 -p 2222:22 --name gintestserver -d gnode/ginhome
