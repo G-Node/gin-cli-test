d3ba5b05ba505364ad0072cd296f7980e070b135
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-07 19:47:01 +0200

    [test all states] Unify tests
    Same test in both modes with a control variable ('mode') for the lock
    state changes.

--- scripts/test-all-states.py
+++ scripts/test-all-states.py
@@ -1,3 +1,10 @@
+"""
+Runs through all possible file states and checks the output of 'gin ls'.
+
+The test is run in both direct and indirect modes. The 'mode' argument is used
+to correctly count the status of files after locking or unlocking in direct
+mode, where (un)locking is a no-op.
+"""
 import os
 import shutil
 from random import randint
@@ -17,7 +24,34 @@ def test_all_states_indirect():
     r.runcommand("gin", "create", reponame,
                  "Test repository for all states (indirect mode)")
     r.cdrel(reponame)
+    run_checks(r, mode=1)
 
+    r.cleanup(reponame)
+    r.logout()
+
+    print("DONE!")
+
+
+def test_all_states_direct():
+    r = Runner()
+    r.login()
+
+    # create repo (remote and local) and cd into directory
+    reponame = f"gin-test-{randint(0, 9999):04}"
+    r.runcommand("gin", "create", reponame,
+                 "Test repository for all states (direct mode)")
+    r.cdrel(reponame)
+    print("************ SWITCHING TO DIRECT MODE ************")
+    r.runcommand("git", "annex", "direct")
+    run_checks(r, mode=0)
+
+    r.cleanup(reponame)
+    r.logout()
+
+    print("DONE!")
+
+
+def run_checks(r, mode):
     # create files in root
     for idx in range(50):
         util.mkrandfile(f"root-{idx}.git", 5)
@@ -49,19 +83,19 @@ def test_all_states_indirect():
 
     # modify all tracked files
     r.runcommand("gin", "unlock", ".")
-    status["UL"] += 20
-    status["OK"] -= 20
+    status["UL"] += 20 * mode
+    status["OK"] -= 20 * mode
     util.assert_status(r, status=status)
     for idx in range(50):
         util.mkrandfile(f"root-{idx}.git", 4)
     for idx in range(70, 90):
         util.mkrandfile(f"root-{idx}.annex", 2100)
     status["OK"] = 0
-    status["MD"] = 50
+    status["MD"] = 50 + 20 * (1 - mode)
     util.assert_status(r, status=status)
 
     r.runcommand("gin", "lock", ".")
-    status["LC"] += 20
+    status["LC"] += 20 * mode
     status["UL"] = 0
     util.assert_status(r, status=status)
 
@@ -106,19 +140,20 @@ def test_all_states_indirect():
     # Unlock some files
     r.runcommand("gin", "unlock", "root-70.annex",
                  "root-75.annex", "root-84.annex")
-    status["UL"] += 3
-    status["OK"] -= 3
+    status["UL"] += 3 * mode
+    status["OK"] -= 3 * mode
     util.assert_status(r, status=status)
 
     # Unlock a whole directory
     r.runcommand("gin", "unlock", "subdir-a")
-    status["UL"] += 10
-    status["OK"] -= 10
+    status["UL"] += 10 * mode
+    status["OK"] -= 10 * mode
     util.assert_status(r, status=status)
 
     # Check subdirectory only
     tenul = zerostatus.copy()
-    tenul["UL"] = 10
+    tenul["UL"] = 10 * mode
+    tenul["OK"] = 10 * (1 - mode)
     util.assert_status(r, path="subdir-a", status=tenul)
 
     # Check again from within the subdir
@@ -128,12 +163,13 @@ def test_all_states_indirect():
 
     # Relock one of the files
     r.runcommand("gin", "lock", "root-84.annex")
-    status["UL"] -= 1
-    status["OK"] += 1
+    status["UL"] -= 1 * mode
+    status["OK"] += 1 * mode
     util.assert_status(r, status=status)
 
     oneul = zerostatus.copy()
-    oneul["UL"] = 1
+    oneul["UL"] = 1 * mode
+    oneul["OK"] = 1 * (1 - mode)
     # Check one of the remaining unlocked files explicitly
     util.assert_status(r, status=oneul, path="root-70.annex")
 
@@ -150,7 +186,8 @@ def test_all_states_indirect():
     # change subdir-a from 'unlocked' to 'no content'
     r.runcommand("gin", "remove-content", "subdir-a")
     status["NC"] += 10
-    status["UL"] -= 10
+    status["UL"] -= 10 * mode
+    status["OK"] -= 10 * (1 - mode)
     util.assert_status(r, status=status)
 
     suba = zerostatus.copy()
@@ -199,199 +236,3 @@ def test_all_states_indirect():
     status["OK"] += status["??"]
     status["??"] = 0
     util.assert_status(r, status=status)
-
-    r.cleanup(reponame)
-    r.logout()
-
-    print("DONE!")
-
-
-def test_all_states_direct():
-    r = Runner()
-    r.login()
-
-    # create repo (remote and local) and cd into directory
-    reponame = f"gin-test-{randint(0, 9999):04}"
-    r.runcommand("gin", "create", reponame,
-                 "Test repository for all states (direct mode)")
-    r.cdrel(reponame)
-    print("************ SWITCHING TO DIRECT MODE ************")
-    r.runcommand("git", "annex", "direct")
-
-    # create files in root
-    for idx in range(50):
-        util.mkrandfile(f"root-{idx}.git", 5)
-    for idx in range(70, 90):
-        util.mkrandfile(f"root-{idx}.annex", 2000)
-
-    status = zerostatus.copy()
-    status["??"] += 70
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "commit", "root*")
-    status["LC"] += 70
-    status["??"] -= 70
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "upload")
-    status["OK"] += status["LC"]
-    status["LC"] = 0
-    util.assert_status(r, status=status)
-
-    # gin upload command should not have created an extra commit
-    assert util.getrevcount(r) == 2
-
-    # Create more root files that will remain UNTRACKED
-    for idx in "abcdef":
-        util.mkrandfile(f"root-file-{idx}.untracked", 1)
-    status["??"] += 6
-    util.assert_status(r, status=status)
-
-    # modify all tracked files
-    r.runcommand("gin", "unlock", ".")
-    # unlock is noop in direct mode
-    util.assert_status(r, status=status)
-    for idx in range(50):
-        util.mkrandfile(f"root-{idx}.git", 4)
-    for idx in range(70, 90):
-        util.mkrandfile(f"root-{idx}.annex", 2100)
-    status["OK"] = 0
-    status["MD"] = 70
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "lock", ".")
-    # lock is noop in direct mode
-    util.assert_status(r, status=status)
-
-    # Upload all except untracked
-    r.runcommand("gin", "upload", "*.annex", "*.git")
-    status["LC"] = 0
-    status["MD"] = 0
-    status["OK"] = 70
-    util.assert_status(r, status=status)
-
-    # Should have 3 commits so far
-    assert util.getrevcount(r) == 3
-
-    # Create some subdirectories with files
-    for idx in "abcdef":
-        dirname = f"subdir-{idx}"
-        os.mkdir(dirname)
-        r.cdrel(dirname)
-        for jdx in range(10):
-            util.mkrandfile(f"subfile-{jdx}.annex", 1500)
-        r.cdrel("..")
-    status["??"] += 60
-    util.assert_status(r, status=status)
-
-    # Upload the files in the first subdirectory only and a couple from the
-    # second
-    r.runcommand("gin", "upload", "subdir-a", "subdir-b/subfile-5.annex",
-                 "subdir-b/subfile-9.annex")
-    status["OK"] += 12
-    status["??"] -= 12
-    util.assert_status(r, status=status)
-    subb = zerostatus.copy()
-    subb["OK"] = 2
-    subb["??"] = 8
-    util.assert_status(r, path="subdir-b", status=subb)
-
-    tenuntracked = zerostatus.copy()
-    tenuntracked["??"] = 10
-    for idx in "cdef":
-        util.assert_status(r, path=f"subdir-{idx}", status=tenuntracked)
-
-    # Noop unlocks again
-    r.runcommand("gin", "unlock", "root-70.annex",
-                 "root-75.annex", "root-84.annex")
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "unlock", "subdir-a")
-    util.assert_status(r, status=status)
-
-    # Check subdirectory only
-    tenok = zerostatus.copy()
-    tenok["OK"] = 10
-    util.assert_status(r, path="subdir-a", status=tenok)
-
-    # Check again from within the subdir
-    r.cdrel("subdir-a")
-    util.assert_status(r, status=tenok)
-    r.cdrel("..")
-
-    # Relock one of the files
-    r.runcommand("gin", "lock", "root-84.annex")
-    util.assert_status(r, status=status)
-
-    oneok = zerostatus.copy()
-    oneok["OK"] = 1
-    # Check one of the files explicitly
-    util.assert_status(r, status=oneok, path="root-70.annex")
-
-    # There should be no NC files so far
-    status["NC"] = 0
-    util.assert_status(r, status=status)
-
-    # Drop some files
-    r.runcommand("gin", "rmc", "subdir-b/subfile-5.annex")
-    status["NC"] += 1
-    status["OK"] -= 1
-    util.assert_status(r, status=status)
-
-    # change subdir-a from 'unlocked' to 'no content'
-    r.runcommand("gin", "remove-content", "subdir-a")
-    status["NC"] += 10
-    status["OK"] -= 10
-    util.assert_status(r, status=status)
-
-    suba = zerostatus.copy()
-    suba["NC"] = 10
-    util.assert_status(r, status=suba, path="subdir-a")
-    subb["OK"] -= 1
-    subb["NC"] += 1
-    util.assert_status(r, status=subb, path="subdir-b")
-
-    # Upload everything and then rmc it
-    r.runcommand("gin", "upload", ".")
-    status["OK"] += status["UL"] + status["MD"] + status["LC"] + status["??"]
-    status["UL"] = status["MD"] = status["LC"] = status["??"] = 0
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "rmc", ".")
-    # annex files are now NC
-    status["NC"] += 69
-    status["OK"] -= 69
-    util.assert_status(r, status=status)
-
-    # remove a few file and check their status
-    os.remove("subdir-a/subfile-1.annex")
-    os.remove("root-10.git")
-    shutil.rmtree("subdir-b")
-    status["RM"] += 12
-    status["NC"] -= 11
-    status["OK"] -= 1
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "commit", ".")
-    status["RM"] = 0
-    util.assert_status(r, status=status)
-
-    # Add new files, remove some existing ones, check status and upload
-    util.mkrandfile("new-annex-file", 10021)
-    util.mkrandfile("new-git-file", 10)
-    shutil.rmtree("subdir-c")
-    status["RM"] += 10
-    status["??"] += 2
-    status["NC"] -= 10
-    util.assert_status(r, status=status)
-
-    r.runcommand("gin", "upload", ".")
-    status["RM"] = 0
-    status["OK"] += status["??"]
-    status["??"] = 0
-    util.assert_status(r, status=status)
-
-    r.cleanup(reponame)
-    r.logout()
-
-    print("DONE!")
