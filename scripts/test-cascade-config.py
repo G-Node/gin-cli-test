87698052eeb571c91bee52880b485e5dfdc9e32d
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-08 15:17:30 +0200

    Port test-cascade-config to Python

---
+++ scripts/test-cascade-config.py
@@ -0,0 +1,66 @@
+import os
+from runner import Runner
+import util
+import pytest
+import yaml
+
+
+@pytest.fixture
+def runner():
+    r = Runner()
+    r.login()
+    # create repo (remote and local) and cd into directory
+    reponame = util.randrepo()
+    print("Setting up test repository")
+    r.runcommand("gin", "create", reponame,
+                 "Test repository for cascading configurations",
+                 echo=False)
+    r.reponame = reponame
+    r.cdrel(reponame)
+
+    yield r
+
+    print(f"Cleaning up {reponame}")
+    r.cleanup(reponame)
+    r.logout()
+
+
+def test_config_path(runner):
+    r = runner
+    conf = dict()
+
+    reporoot = os.path.join(r.testroot.name, r.reponame)
+    localconffile = os.path.join(reporoot, "config.yml")
+
+    def writelocalconf():
+        with open(localconffile, "w") as conffile:
+            conffile.write(yaml.dump(conf, default_flow_style=False))
+
+    # Create local config file which sets annex threshold to 0kb
+    conf["annex"] = {"minsize": "0kB"}
+    writelocalconf()
+
+    # small files should now be added to annex
+    util.mkrandfile("smallfile", 1)
+    r.runcommand("gin", "upload", "smallfile")
+    # smallfile should be a symlink
+    assert os.path.islink("smallfile")
+
+    # .md file should still be excluded because of the exclusion rule in the
+    # global configuration
+    util.mkrandfile("anotherfile.md", 10)
+    r.runcommand("gin", "upload", "anotherfile.md")
+    # anotherfile.md should not be a symlink
+    assert not os.path.islink("anotherfile.md")
+
+    # config file should never be added to annex
+    r.runcommand("gin", "upload", "config.yml")
+    assert not os.path.islink("config.yml")
+
+    # changing gitannex binary in local configuration should have no effect
+    conf["bin"] = {"gitannex": "ls"}
+    writelocalconf()
+
+    util.mkrandfile("somefile", 1000)
+    r.runcommand("gin", "commit", ".")
+    r.runcommand("gin", "upload", ".")
