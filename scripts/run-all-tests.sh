8af4376c7f6ec61c8a20d9b0cdfa1e73567e2378
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-08 17:59:03 +0200

    Rename tests to be discoverable
    Use underscores instead of minus for proper Python names.
    Simply run everything using pytest.

--- scripts/run-all-tests.sh
+++ scripts/run-all-tests.sh
@@ -12,45 +12,15 @@ fi
 errorcount=0
 errored=()
 
-for testscript in test-*.sh
-do
-    ./$testscript
-    teststatus=$?
-    if [ $teststatus -ne 0 ]
-    then
-        echo "Test $testscript failed"
-        errorcount=$((errorcount+1))
-        errored+=("$testscript")
-    fi
-done
-
-for testscript in test-*.py
-do
-    # Not the most efficient, since we could just do pytest *.py
-    # but helps with pointing out failures.
-    # Will replace with single command when all tests are pytests.
-    pytest $testscript
-    teststatus=$?
-    if [ $teststatus -ne 0 ]
-    then
-        echo "Test $testscript failed"
-        errorcount=$((errorcount+1))
-        errored+=("$testscript")
-    fi
-done
+pytest
+teststatus=$?
 
 ./delete-all-test-repos.sh <<< echo
 ./logout.sh
 
-if [ $errorcount -gt 0 ]
+if [ $teststatus -gt 0 ]
 then
-    echo "------------"
-    echo "Tests failed"
-    echo "------------"
-    for testscript in "${errored[@]}"
-    do
-        echo $testscript
-    done
+    echo "Failed"
     exit 1
 fi
 
