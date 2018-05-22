9d0e4959fb1fcbdff4bfd805b4cd3e034b6068d9
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-22 11:29:43 +0200

    [error tests] Check for new style error messages

--- scripts/test_error_conditions.py
+++ scripts/test_error_conditions.py
@@ -29,7 +29,7 @@ def runner():
 
 def test_errors(runner):
     r = runner
-    norepoerr = "E: This command must be run from inside a gin repository."
+    norepoerr = "[error] this command must be run from inside a gin repository"
 
     commands = [
         "upload",
@@ -61,7 +61,7 @@ def test_errors(runner):
     for cmd in commands[1:]:
         out, err = r.runcommand("gin", cmd, "foobar", exit=False)
         assert out == "No files matched foobar"
-        assert err == "E: 1 operation failed"
+        assert err == "[error] 1 operation failed"
 
     # make a few annex and git files
     os.mkdir("smallfiles")
@@ -107,7 +107,7 @@ def test_errors(runner):
     errlines = err.splitlines()
     for line in errlines[:-1]:
         assert line.strip().endswith("Content not available locally")
-    assert errlines[-1].strip() == "E: 5 operations failed"
+    assert errlines[-1].strip() == "[error] 5 operations failed"
 
     # change git repo remote address/port and test get-content failure
     out, err = r.runcommand("git", "remote", "-v")
@@ -120,7 +120,7 @@ def test_errors(runner):
     errlines = err.splitlines()
     for line in errlines[:-1]:
         assert line.strip().endswith("(content or server unavailable)")
-    assert errlines[-1].strip() == "E: 5 operations failed"
+    assert errlines[-1].strip() == "[error] 5 operations failed"
 
     # revert remote change
     address = address.replace("1", "22")
@@ -141,7 +141,7 @@ def test_errors(runner):
         conffile.write(yaml.dump(confdata))
     out, err = r.runcommand("gin", "create", "ThisShouldFail", exit=False)
     assert err, "Expected error, got nothing"
-    errmsg = "E: server refused connection"
+    errmsg = "[error] server refused connection"
     assert err == errmsg
 
     # TODO: simulate not-enough-free-space
@@ -161,19 +161,19 @@ def test_errors(runner):
     for line in errlines[:-1]:
         # TODO: Should print auth error
         assert line.strip().endswith("(content or server unavailable)")
-    assert errlines[-1].strip() == "E: 5 operations failed"
+    assert errlines[-1].strip() == "[error] 5 operations failed"
 
     out, err = r.runcommand("gin", "download", exit=False)
     assert err, "Expected error, got nothing"
     errlines = err.splitlines()
     assert len(errlines) == 1
-    assert errlines[0].strip() == "E: download failed: permission denied"
+    assert errlines[0].strip() == "[error] download failed: permission denied"
 
     out, err = r.runcommand("gin", "upload", exit=False)
     assert err, "Expected error, got nothing"
     errlines = err.splitlines()
     assert len(errlines) == 1
-    assert errlines[0].strip() == "E: 1 operation failed"
+    assert errlines[0].strip() == "[error] 1 operation failed"
     outlines = out.splitlines()
     assert outlines[-1].strip() == "upload failed: permission denied"
 
@@ -193,7 +193,7 @@ def test_errors(runner):
     errlines = err.splitlines()
     assert len(errlines) == 1
     assert errlines[0].strip() ==\
-        "E: download failed: server key does not match known host key"
+        "[error] download failed: server key does not match known host key"
 
     out, err = r.runcommand("gin", "get-content", "datafiles", exit=False)
     assert err, "Expected error, got nothing"
@@ -201,13 +201,13 @@ def test_errors(runner):
     for line in errlines[:-1]:
         # TODO: Should print host key error
         assert line.strip().endswith("(content or server unavailable)")
-    assert errlines[-1].strip() == "E: 5 operations failed"
+    assert errlines[-1].strip() == "[error] 5 operations failed"
 
     out, err = r.runcommand("gin", "upload", exit=False)
     assert err, "Expected error, got nothing"
     errlines = err.splitlines()
     assert len(errlines) == 1
-    assert errlines[0].strip() == "E: 1 operation failed"
+    assert errlines[0].strip() == "[error] 1 operation failed"
     outlines = out.splitlines()
     assert outlines[-1].strip() ==\
         "upload failed: server key does not match known host key"
@@ -219,8 +219,8 @@ def test_errors(runner):
     r.cdrel("..")
 
     out, err = r.runcommand("gin", "create", r.reponame, exit=False)
-    assert err == ("E: invalid repository name or repository with the same "
-                   "name already exists")
+    assert err == ("[error] invalid repository name or "
+                   "repository with the same name already exists")
 
     # Creating repository but local directory already exists (non-empty)
     anotherrepo = f"gin-test-{randint(0, 9999):04}"
@@ -235,7 +235,7 @@ def test_errors(runner):
         f"Repository download failed.\n'{anotherrepo}' already exists in the "
         "current directory and is not empty."
     )
-    assert err == "E: 1 operation failed"
+    assert err == "[error] 1 operation failed"
 
     out, err = r.runcommand("gin", "repos")
     assert anotherrepo in out
