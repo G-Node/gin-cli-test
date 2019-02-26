4d48eb1be45994f7fac84ddccb14ccc0022848bc
Achilleas Koutsou <achilleas.k@gmail.com>
2019-02-26 18:40:02 +0100

    [setenv] Copy config to tmpdir for shell scripts

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -10,8 +10,13 @@ mkannexfile() {
     dd if=/dev/urandom of=$1 bs=100k count=1
 }
 
-export GIN_CONFIG_DIR="${scriptsloc}/../conf"
+
+export GIN_CONFIG_DIR="$(mktemp -d)"
 export GIN_LOG_DIR="${scriptsloc}/../log"
+
+defaultconf="${scriptsloc}/../conf/config.yml"
+mkdir -p ${GIN_CONFIG_DIR}
+cp ${defaultconf} ${GIN_CONFIG_DIR}
 
 username=testuser
 password="a test password 42"
